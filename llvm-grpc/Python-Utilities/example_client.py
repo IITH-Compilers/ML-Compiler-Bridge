3a33696dd58143730c0116567a9250afd71d1c09
pranav-159 <cs20btech11018@iith.ac.in>
2023-06-06 22:29:32 +0530

    added server code related to poset-rl training in poset-rl pass and a sample python client to test the server

---
+++ llvm-grpc/Python-Utilities/example_client.py
@@ -0,0 +1,56 @@
+import grpc
+from multiprocessing import Process, Queue
+import subprocess
+import os
+import signal
+import time
+
+import example_pb2_grpc
+import example_pb2
+
+from google.protobuf.json_format import MessageToJson
+import json
+import numpy as np
+
+def repeatedgRPCFieldToNumpyArray(gRPCObj):
+    jsonObj = MessageToJson(gRPCObj)
+    dictObj = json.loads(jsonObj)
+    array = dictObj['embedding']
+    return np.array(array)
+
+
+class PosetRLClient(object):
+
+    def __init__(self):
+
+        self.host = '127.0.0.1'
+        self.server_port = 50051
+
+        self.process = None
+
+        self.channel = grpc.insecure_channel(
+            '{}:{}'.format(self.host, self.server_port))
+
+        self.stub = example_pb2_grpc.PosetRLStub(self.channel)
+
+    def applyActionGetEmbeddings(self, action):
+
+        request = example_pb2.ActionRequest(action=action)
+
+        response = self.stub.applyActionGetEmbeddings(request)
+
+        print(type(response))
+        print(response)
+        # responseInArray = repeatedgRPCFieldToNumpyArray(response)
+
+        # print(type(responseInArray[1]))
+        # print(responseInArray)
+
+
+
+if __name__ == '__main__':
+
+    client = PosetRLClient()
+    client.applyActionGetEmbeddings(-1)
+
+    # print(client.applyActionGetEmbeddings(3))
