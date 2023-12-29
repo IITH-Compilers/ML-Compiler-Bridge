1a6d06f706008eaac9cc44d4fd46fbef34b7abf3
Rajiv Chitale <rajivchitale@gmail.com>
2023-12-29 21:45:33 +0530

    Moved python CompilerInterface, SerDes to submodule

---
+++ CompilerInterface/GrpcCompilerInterface.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+from BaseCompilerInterface import BaseCompilerInterface
+import os
+import io
+
+import sys
+# sys.path.append('/Pramana/ML_LLVM_Tools/ml-llvm-project/ml-llvm-tools/MLModelRunner/gRPCModelRunner/Python-Utilities/')
+# import posetRL_pb2_grpc, posetRL_pb2
+import grpc
+from concurrent import futures
+
+# requires grpc stub evaluate, service_server obj and adder method
+
+class GrpcCompilerInterface(BaseCompilerInterface):
+    def __init__(self, mode, stub_class=None, hostip='127.0.0.1', hostport=50051, add_server_method=None,grpc_service_obj=None ):
+        super().__init__('protobuf')
+        self.mode = mode
+
+        self.host=hostip
+        self.server_port = hostport
+
+        # print('address ', '{}:{}'.format(self.host,self.server_port))
+
+        if self.mode == 'client':
+            # self.process=None
+            self.channel = grpc.insecure_channel('{}:{}'.format(self.host,self.server_port))
+            print("Setting stub", stub_class)
+            self.stub = stub_class(self.channel)
+        
+        elif self.mode == 'server':
+            self.grpc_service_obj = grpc_service_obj
+            self.add_server_method = add_server_method
+            self.start_server()
+
+        # self.read_stream_iter = None        
+
+    def __del__(self):
+        pass
+
+    # def populate_buffer(self, data):
+    #     self.serdes_obj.serializeData(data)
+
+    def evaluate(self, mode = None):
+        out = self.serdes_obj.getOutputBuffer()
+        # print("Stub class:", type(self.stub.queryCompiler))
+        return self.stub.queryCompiler(out)
+
+
+    def start_server(self):
+        # ray.init()
+        if self.mode == 'server':
+            server=grpc.server(futures.ThreadPoolExecutor(max_workers=20),options = [
+                        ('grpc.max_send_message_length', 200*1024*1024), #50MB
+                                ('grpc.max_receive_message_length', 200*1024*1024) #50MB
+                                    ])
+
+            # RegisterAllocationInference_pb2_grpc.add_RegisterAllocationInferenceServicer_to_server(service_server(inference_obj),server)
+            self.add_server_method(self.grpc_service_obj,server)
+            # server.add_insecure_port('localhost:' + str(sys.argv[1]))
+            server.add_insecure_port('{}:{}'.format(self.host,self.server_port))
+
+            server.start()
+            print("Server Running")        
+            server.wait_for_termination()
+
+        # elif self.mode == 'client':
---
+++ CompilerInterface/PipeCompilerInterface.py
@@ -0,0 +1,95 @@
+from abc import ABC, abstractmethod
+from BaseCompilerInterface import BaseCompilerInterface
+import os
+import io
+
+class PipeCompilerInterface(BaseCompilerInterface):
+
+    def __init__(self, data_format=None, pipe_name=None):
+        super().__init__(data_format)
+        self.pipe_name = pipe_name
+        self.to_compiler = None
+        self.from_compiler = None
+        self.tc = None
+        self.fc = None
+        self.buffer = None
+        self.init_pipes()
+        # self.read_stream_iter = None        
+
+    def __del__(self):
+        self.close_pipes()
+        self.remove_pipes()
+
+    # def populate_buffer(self, data):
+    #     self.serdes_obj.serializeData(data)
+
+    # writes buffer, then reads from pipe
+    def evaluate(self, mode = None):
+        out = self.serdes_obj.getOutputBuffer()
+        if out is not None:
+            self.tc.write(out)
+            self.tc.flush()
+
+        if mode == 'exit':
+            return None
+
+        result = self.serdes_obj.deserializeData(self.fc)
+
+        # self.close_pipes()
+        # self.init()
+        return result
+
+    # INIT PIPE COMMN
+
+    # def init_pipe_communication(self):
+    #     self.init_pipes()
+    #     self.init_buffers_communication()
+    #     self.read_stream_iter = None
+
+    def init_pipes(self):
+        # print("Init pipes")
+        self.to_compiler = self.pipe_name + ".in"
+        self.from_compiler = self.pipe_name + ".out"
+        if os.path.exists(self.to_compiler):
+            os.remove(self.to_compiler)
+        if os.path.exists(self.from_compiler):
+            os.remove(self.from_compiler)
+
+        os.mkfifo(self.to_compiler, 0o666)
+        os.mkfifo(self.from_compiler, 0o666)
+
+    def reset_pipes(self):
+        self.tc = io.BufferedWriter(io.FileIO(self.to_compiler, "wb"))
+        self.fc = io.BufferedReader(io.FileIO(self.from_compiler, "rb"))
+
+    def close_pipes(self):
+        if self.fc is not None:
+            self.tc.close()
+            self.fc.close()
+            self.tc = None
+            self.fc = None
+
+    def remove_pipes(self):
+        os.remove(self.to_compiler)
+        os.remove(self.from_compiler)
+
+    # COMMUNICATE WITH PIPES
+
+
+
+    # def evaluate_untyped(self):
+    #     pass 
+    
+        # def send(self, data):
+    #     out = serdes_obj.serializeData(data)
+    #     self.init()
+    #     self.tc.write(out)
+    #     self.tc.flush()
+
+    # def receive(self, data):
+    #     hdr = self.fc.read(8)
+    #     size = int.from_bytes(hdr, "little")
+    #     data = self.fc.read(size)
+    #     result = serdes_obj.deserializeData(data7)
+    #     self.close_pipes()
+    #     return result
