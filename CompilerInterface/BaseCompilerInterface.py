019a5d8d10d00a6fc6cb50e8d1e495016e0b4ea1
Rajiv Chitale <rajivchitale@gmail.com>
2024-01-09 00:00:53 +0530

    Trying Doxygen comments with BaseCompilerInterface

--- CompilerInterface/BaseCompilerInterface.py
+++ CompilerInterface/BaseCompilerInterface.py
@@ -3,15 +3,18 @@ from SerDes import SerDes
 import os
 import io
 
+## This base class specifies methods for communication with compiler.
 class BaseCompilerInterface(ABC):
-
+    ## Initializes correct SerDes object
     def __init__(self, data_format=None):
         self.serdes_obj = SerDes(data_format) 
 
+    ## Sets content of next request into a buffer.
     def populate_buffer(self, data):
         self.serdes_obj.serializeData(data)
 
     @abstractmethod
+    ## Sends query to compiler and returns result.
     def evaluate(self):
         pass
 
