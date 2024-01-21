495e7b637df7699639ac426732caba770371e688
Umesh-k26 <umeshkalvakuntla26@gmail.com>
2024-01-21 12:08:49 +0530

    MLConfig path to be defined by client

--- MLModelRunner/Utils/MLConfig.cpp
+++ MLModelRunner/Utils/MLConfig.cpp
@@ -7,7 +7,3 @@
 //===----------------------------------------------------------------------===//
 
 #include "MLModelRunner/Utils/MLConfig.h"
-
-llvm::cl::opt<std::string> MLBridge::MLConfig::mlconfig(
-    "ml-config-path", llvm::cl::Hidden, llvm::cl::Optional,
-    llvm::cl::desc("Path to ML config files"), llvm::cl::init(""));
